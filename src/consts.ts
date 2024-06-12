c44de8c7f2d432bc4716bfa4282e525fbc85f376
Oli Evans <oli@protocol.ai>
2024-06-12 14:48:24 +0100

    feat: ✨🎷🐩
    License: MIT
    Signed-off-by: Oli Evans <oli.evans@gmail.com>

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sanda Academy';
+export const SITE_DESCRIPTION = `Women's self defence classes`;
+
