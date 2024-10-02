325d3a05ecd2f06926b1b3ee9b4134b8f3762a40
Oli Evans <oli@protocol.ai>
2024-10-02 14:55:13 +0100

    fix: self defence is for all!
    License: MIT
    Signed-off-by: Oli Evans <oli@zilla.org.uk>

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sanda Academy';
-export const SITE_DESCRIPTION = `Women's self defence classes`;
+export const SITE_TITLE = 'Brixton Self-Defence';
+export const SITE_DESCRIPTION = `Practical self-defence classes. Open to all. Based in Brixton, South London.`;
 
